10fdc01eeb861c2b8b42ae2201f82b1af0702f46
Alex Dusenbery <adusenbery@edx.org>
2018-01-22 11:34:07 -0500

    Initial commit, generated boilerplate from cookiecutter repo.

---
+++ tests/test_models.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Tests for the `completion` models module.
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+
+class TestBlockCompletion(object):
+    """
+    Tests of the BlockCompletion model.
+    """
+
+    def test_something(self):
+        """TODO: Write real test cases."""
+        pass
